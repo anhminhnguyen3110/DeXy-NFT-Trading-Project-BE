0737466bd2ba8cdc675c7269ac20712c78708c0d
emyeucanha5 <nganhminh2003@gmail.com>
2023-09-13 03:15:33 +1000

    fix: [IN-60] fix api docs and mitigate pycache

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from loguru import logger
 from config.core import Setting
 from utils.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
-
 from routers import user_router
-
+import sys
+sys.dont_write_bytecode = True
 
 def create_table():
   logger.debug('Creating Tables')
@@ -13,14 +13,17 @@ def create_table():
   
 def include_router(app: FastAPI):
   logger.debug('Including Routers')
-  app.include_router(user_router.router, tags=['user'])
+  app.include_router(user_router.router, tags=['User'], prefix='/api/v1')
   
 def start_application():
   app = FastAPI(
-    title = Setting.PROJECT_NAME,
-    version = Setting.PROJECT_VERSION,
-    root_path=Setting.API_ROOT_PATH,
+    title=Setting.PROJECT_NAME,
+    version=Setting.PROJECT_VERSION,
   )
+  @app.get("/")
+  async def test():
+    return {"message": "This is the / endpoint"}
+  
   create_table()
   include_router(app)
   
