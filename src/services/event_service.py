b6d57b5dbd86a5893c33748e2fa67e325cfc1ec7
emyeucanha5 <nganhminh2003@gmail.com>
2023-10-14 15:52:13 +1100

    fix: [IN-142] shopping cart deletion

--- src/services/event_service.py
+++ src/services/event_service.py
@@ -1,5 +1,7 @@
+from models.shopping_cart_item_model import ShoppingCartItemModel
 from models.transaction_model import TransactionModel
 from repositories.item_repository import ItemRepository
+from repositories.shopping_cart_item_repository import ShoppingCartItemRepository
 from repositories.transaction_repository import TransactionRepository
 from repositories.user_repository import UserRepository
 from schemas.event.event_dto import TransactionItem
@@ -12,6 +14,7 @@ class EventService:
         self.item_repo = ItemRepository(self.db)
         self.user_repo = UserRepository(self.db)
         self.transaction_repo = TransactionRepository(self.db)
+        self.shopping_cart_repo = ShoppingCartItemRepository(self.db)
 
     async def handler(self, event: TransactionItem):
         transaction_smart_contract_id = event.transactionId
@@ -21,8 +24,10 @@ class EventService:
 
         owner = self.user_repo.get_user_by_wallet_address(owner)
         buyer = self.user_repo.get_user_by_wallet_address(buyer)
+        shopping_cart_item = self.shopping_cart_repo.get_shopping_cart_item_by_user_id_and_item_id(
+            buyer.user_id, item_id
+        )
         item = self.item_repo.get_item_by_id_maintain_session(item_id)
-
         if owner is None or buyer is None:
             print("Owner or buyer not found")
             return
@@ -42,6 +47,10 @@ class EventService:
             )
 
             item.item_owner_id = buyer.user_id
+            if(shopping_cart_item is not None):
+                self.db.query(ShoppingCartItemModel).filter(
+                    ShoppingCartItemModel.shopping_cart_item_id == shopping_cart_item.shopping_cart_item_id
+                ).delete()
             self.db.commit()
             print("Transaction added successfully")
         except Exception as e:
